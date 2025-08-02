56f9cd14d62a9598b3c5474c62e9502eef5153e5
cintyy73 <cintiapaezdm@gmail.com>
2025-08-02 14:54:37 -0300

    fix: update logo in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+
+import { Box, Flex, Heading, Text, Avatar } from '@chakra-ui/react';
+// Usar el logo nuevo
+const logo = '/assets/logo_01.jpg';
 
 const Header = () => (
   <Flex align="center" gap={3}>
     <Box display="flex" alignItems="center" gap={3}>
-      <img src="/assets/logo.png" alt="Logo Gonzalo" style={{ width: 60, height: 60, borderRadius: '50%' }} />
+      <Avatar src={logo} name="Logo Gonzalo" boxSize={"80px"} />
       <Box>
         <Heading size="md" color="gray.900">Gonzalo</Heading>
         <Text fontSize="sm" color="gray.600" fontWeight="medium">BARBERÍA</Text>
